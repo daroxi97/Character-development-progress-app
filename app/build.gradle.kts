b25abe5e7c0e826f33051371c13be0102c0f9512
DanRoeniger <danfeina97@gmail.com>
2024-03-05 05:05:38 +0100

    Removed extra dependencies from gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,56 +68,18 @@ android {
 }
 
 dependencies {
-
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
-    implementation("androidx.activity:activity-ktx:1.8.2")
-    implementation("androidx.fragment:fragment-ktx:1.6.2")
-
     implementation("com.google.dagger:hilt-android:2.48")
     kapt("com.google.dagger:hilt-android-compiler:2.48")
     implementation ("androidx.hilt:hilt-navigation-compose:1.2.0")
-
-
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
-    implementation ("com.google.android.material:material:1.11.0")
-
-    implementation ("androidx.compose.runtime:runtime")
-    implementation ("androidx.compose.foundation:foundation")
-    implementation ("androidx.compose.material:material")
     implementation ("androidx.compose.material:material-icons-extended")
-    implementation ("androidx.activity:activity-compose:1.8.2")
     implementation ("androidx.room:room-ktx:2.6.1")
-    implementation ("androidx.lifecycle:lifecycle-common-java8:2.7.0")
-
-    implementation ("android.arch.persistence.room:runtime:1.1.1")
-    kapt ("android.arch.persistence.room:compiler:1.1.1")
     kapt ("androidx.room:room-compiler:2.6.1")
     implementation ("androidx.navigation:navigation-compose:2.8.0-alpha03")
-
-
-    implementation ("androidx.datastore:datastore-preferences:1.0.0")
-    implementation ("androidx.datastore:datastore:1.0.0")
-
-    implementation ("androidx.navigation:navigation-compose:2.7.7")
-
     implementation ("androidx.compose.material3:material3:1.3.0-alpha01")
-
-
     implementation("androidx.compose.runtime:runtime-livedata")
 
-
-
-
-
     debugImplementation ("androidx.compose.ui:ui-tooling")
-
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
